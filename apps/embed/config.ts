1d85184251bc3898047b5309afd44ef5016c2b6d
Antonov-S <anto1nov1@gmail.com>
2025-11-27 03:18:22 +0200

    35: deployment

--- apps/embed/config.ts
+++ apps/embed/config.ts
@@ -1,5 +1,3 @@
-import { DEFAULT_CIPHERS } from "tls";
-
 export const EMBED_CONFIG = {
   WIDGET_URL: import.meta.env.VITE_WIDGET_URL || "http://localhost:3001",
   DEFAULT_ORG_ID: "org_35OueDMvTb3K58fO0z1NfFd0QCu",
